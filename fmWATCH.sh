2a6f976e421d22235e9b16803a6296daf7cba8cb
Aaron Polley <aarondavidpolley@gmail.com>
2017-09-01 00:40:02 +1000

    v0.05

--- fmWATCH.sh
+++ fmWATCH.sh
@@ -2,20 +2,23 @@
 
 ################################################################################
 # Author:    Aaron Polley                                                      #
-# Date:      31/08/2017                                                        #
-# Version:   0.01                                                              #
+# Date:      1/09/2017                                                        #
+# Version:   0.05                                                              #
 # Purpose:   Scripting for monitoring and resolving false mounts               #
 #            Should be triggered by LaunchAgent using WatchPaths               #
 ################################################################################
 
 #---Variables and such---#
-script_version="0.01"
+script_version="0.05"
 user_id=`id -u`
 user_name=`id -un $user_id`
 home_dir=`dscl . read /Users/"$user_name" NFSHomeDirectory | awk '{print $2}'`
-log_file="$home_dir/Library/Logs/fmWATCH.log"
+log_file="/Library/Logs/fmWATCH.log"
 os_vers=`sw_vers -productVersion | awk -F "." '{print $2}'`
 DateTime=`date "+%a %b %d %H:%M:%S"`
+currentUser=`/usr/bin/stat -f%Su /dev/console`
+
+
 
 #---Redirect output to log---#
 exec >> $log_file 2>&1
@@ -31,23 +34,32 @@ echo "$DateTime     - User:              $user_name"
 echo "$DateTime     - User ID:           $user_id"
 echo "$DateTime     - Home Dir:          $home_dir"
 echo "$DateTime     - OS Vers:           10.${os_vers}"
+echo "$DateTime     - LoadUser:          $currentUser"
 
 #--Slow down and let things legitimately mount---#
 
-echo "$DateTime - Pausing for 10 seconds..."
+#echo "$DateTime - Pausing for 10 seconds..."
 
-sleep 10
+#sleep 10
 
 echo "$DateTime - Building list of false mounts..."
 
-falseMounts=`ls -l /Volumes/ | grep wheel | grep aaron | awk '{print $9,$10,$11,$12,$13}' | awk '{$1=$1};1'`
+falseMounts=`ls -l /Volumes/ | grep wheel | grep $currentUser | awk '{print $9,$10,$11,$12,$13}' | awk '{$1=$1};1'`
+
+#---Adjust for loop delimiter---#
+SAVEIFS=$IFS
+IFS=$'\n'
 
 for fmount in $falseMounts ; do
 
    fmount_point=$(echo "/Volumes/$fmount")
-   echo $fmount_point
+   echo "Removing '"$fmount_point"'"
+   rmdir $fmount_point
    
 done
+
+#---Restore IFS---#
+IFS=$SAVEIFS
 
 echo "$DateTime - Complete..."
 
